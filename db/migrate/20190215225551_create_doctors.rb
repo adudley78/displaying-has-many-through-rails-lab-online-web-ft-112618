9334cfd52722b7f831b28517e1dfe1d41f9b9ebf
adudley78 <dudleyadam@gmail.com>
2019-02-15 19:26:16 -0500

    Done.

---
+++ db/migrate/20190215225551_create_doctors.rb
@@ -0,0 +1,11 @@
+class CreateDoctors < ActiveRecord::Migration
+  def change
+    create_table :doctors do |t|
+      t.string :name
+      t.string :department
+      t.references :patient, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
